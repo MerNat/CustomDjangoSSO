31ce69ecada7c99d91773e45f880e963b3be59cb
mernat <mernat777@gmail.com>
2019-01-16 12:56:22 +0300

    feat: Using abstract Meta in Models

--- studio/models.py
+++ studio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-
+    . fields.
+    updating ``created`` and ``modified``
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    
+    class Meta:
+        abstract = True
