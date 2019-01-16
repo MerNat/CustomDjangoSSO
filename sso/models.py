8c84bc9d6c17f3b52c71e43e2b5caddbf595bbee
mernat <mernat777@gmail.com>
2019-01-16 16:10:26 +0300

    fix: auth app has already been used by DJ

--- sso/models.py
+++ sso/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from .core.absmodels import *
 
-# Create your models here.
-
 class Studio(DateTimeStampedModel,AbstractStampedModel):
     name = models.CharField(max_length=40)
