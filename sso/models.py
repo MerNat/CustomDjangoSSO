532780af0920206a520ebb8a2ca47f5044e71489
mernat <mernat777@gmail.com>
2019-01-20 18:54:27 +0300

    feat: JWT Added in settings

--- sso/models.py
+++ sso/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from .core.absmodels import *
 
 class User(DateTimeStampedModel):
+    USER_TYPE = (
+        ('ADM', 'Administrator'),
+        ('MEM', 'Member')
+    )
     id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=40,null=False)
     last_name = models.CharField(max_length=40,null=False)
+    user_typ = models.CharField(max_length=3,default='MEM')
     email = models.EmailField(unique=True, max_length=100, null=False)
     password = models.CharField(max_length=200, null=False)
