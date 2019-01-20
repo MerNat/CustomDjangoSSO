b74c69737ec29ef0fae8456fec99a804c83c60cf
mernat <mernat777@gmail.com>
2019-01-20 23:23:38 +0300

    refactor: Register Class Accepts

--- sso/urls.py
+++ sso/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+# from django.urls import path
 from sso import views
 from rest_framework.routers import DefaultRouter
+from django.conf.urls import url
 
 my_router = DefaultRouter()
 
 my_router.register(r'register', views.RegisterUserViewset, base_name='Register')
 
-# urlpatterns = [
-    
-# ]
+urlpatterns = my_router.urls
