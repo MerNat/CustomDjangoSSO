c0e3389f0a56400b7e4a7b63e67d3cbfe192e41c
mernat <mernat777@gmail.com>
2019-01-20 22:25:32 +0300

    feat: Register View+Url added

--- sso/urls.py
+++ sso/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from sso import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+my_router = DefaultRouter()
+
+my_router.register(r'register', views.RegisterUserViewset, base_name='Register')
+
+# urlpatterns = [
     
-]
+# ]
--- sso/views.py
+++ sso/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
+from rest_framework import viewsets, status
+from .serializers import *
+from .models import *
 
 # Create your views here.
+
+class RegisterUserViewset(viewsets.ModelViewSet):
+    """ Registers a User """
+    # queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, format=None):
+        
+        return Response(status=status.HTTP_200_OK)
 
 
