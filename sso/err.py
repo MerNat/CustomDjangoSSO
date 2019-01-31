f9241c1d832607e756aef330a64d1db79ebb20e4
mernat <mernat777@gmail.com>
2019-01-31 22:59:20 +0300

    feat: Regi Done

--- sso/err.py
+++ sso/err.py
@@ -1,3 +1,13 @@
 ## Defines and returns the exception.
 
-import os
+import sys
+import linecache
+
+def MyErrorException(Err, Name):
+    tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_frame
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename,lineno, f.f_globals)
+    print("{}: {}, {},{}".format(Name, Err, filename, lineno))
--- sso/views.py
+++ sso/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework import viewsets, status
+from django.contrib.auth.hashers import make_password, check_password
+from .err import MyErrorException
 from .serializers import (
     RegisterSerializer,
     LoginSerializer
@@ -18,10 +20,25 @@ class RegisterUserViewset(viewsets.ModelViewSet):
     serializer_class = RegisterSerializer
 
     def create(self, request, format=None):
-        data = request.data
-        f_name = data['f_name']
-        l_name = data['l_name']
-        email = data['email']
-        return Response(status=status.HTTP_200_OK)
+        try:
+            serializer = RegisterSerializer(request.data)
+            if not serializer.is_valid():
+                raise Exception(serializer.errors)
+            data = request.data
+            f_name = data['f_name']
+            l_name = data['l_name']
+            email = data['email']
+            password = data['password']
+            user_creation = User.objects.create(
+                f_name = f_name,
+                l_name = l_name,
+                email = email,
+                password = make_password(password)
+            )
+        except Exception as Err:
+            MyErrorException(Err, "Registering User")
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = RegisterSerializer(user_creation)    
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
